84d06ee38ace338635be80649e85ae19b3875507
Udhayakumar S <102042728+udhaymsd@users.noreply.github.com>
2023-05-28 16:00:23 +0530

    Add files via upload

---
+++ Async-callback-Day 1/script.js
@@ -0,0 +1,34 @@
+setTimeout(()=>{
+    document.getElementById("timer").innerHTML=10;
+    setTimeout(()=>{
+        document.getElementById("timer").innerHTML=9;
+        setTimeout(()=>{
+            document.getElementById("timer").innerHTML=8;
+            setTimeout(()=>{
+                document.getElementById("timer").innerHTML=7;
+                setTimeout(()=>{
+                    document.getElementById("timer").innerHTML=6;
+                    setTimeout(()=>{
+                        document.getElementById("timer").innerHTML=5;
+                        setTimeout(()=>{
+                            document.getElementById("timer").innerHTML=4;
+                            setTimeout(()=>{
+                                document.getElementById("timer").innerHTML=3;
+                                setTimeout(()=>{
+                                    document.getElementById("timer").innerHTML=2;
+                                    setTimeout(()=>{
+                                        document.getElementById("timer").innerHTML=1;
+                                        setTimeout(()=>{
+                                            document.getElementById("timer").innerHTML="Happy Independence Day";
+                                            
+                                        },1000)
+                                    },1000)
+                                },1000)
+                            },1000)
+                        },1000)
+                    },1000)
+                },1000)
+            },1000)
+        },1000)
+    },1000)
+})
